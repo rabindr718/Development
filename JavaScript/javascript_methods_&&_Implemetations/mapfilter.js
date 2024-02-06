71fc64165e3df2a1b0c551a4a108e93d08d248db
rabindr718 <rabindraksharma19@gmail.com>
2024-02-06 23:32:39 +0530

    date 6feb

---
+++ JavaScript/javascript_methods_&&_Implemetations/mapfilter.js
@@ -0,0 +1,11 @@
+console.log("Hello Rabindra");
+
+// const name = (document.getElementById("name").innerHTML = "The Rabindra");
+// name.style.color = "red";
+const array = [5, 10, 3, 6];
+const result = array.map((x) => {
+  return x * 2;
+});
+console.log(result);
+// console.log(index);
+console.log(array);
