89138af53594424254e26b927e44654cb2e9d492
rabindr718 <rabindraksharma19@gmail.com>
2024-02-09 23:19:34 +0530

    9 feb 2024

---
+++ WebApplication/springrest/src/main/java/com/springrest/springrest/Controller/MyController.java
@@ -0,0 +1,12 @@
+package com.springrest.springrest.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	public String name() {
+		return "Rabindra";
+	}
+	
+
+}
