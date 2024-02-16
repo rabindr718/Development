4b51c02bd3b22b54f9734556ecb7bb552f994933
rabindr718 <rabindraksharma19@gmail.com>
2024-02-16 23:15:13 +0530

    Add Files

--- JavaScript/AyncAwait/async.js
+++ JavaScript/AyncAwait/async.js
@@ -1,21 +1,3 @@
-const data = [
-  {
-    name: "Rabindra",
-    Profession: "Student",
-    Roll: "137",
-  },
-  {
-    name: "Manish",
-    Profession: "Student",
-    Roll: "128",
-  },
-  {
-    name: "Rajkumar",
-    Profession: "Student",
-    Roll: "139",
-  },
-];
-
 function getData() {
   setTimeout(() => {
     let output = "";
