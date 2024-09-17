24adb4e6b81eaddd92daae709eb0b940cfb8422c
rabindr718 <rabindraksharma19@gmail.com>
2024-09-17 23:45:40 +0530

    17.08.2024

---
+++ NET c#/ecommerce-site/Program.cs
@@ -0,0 +1,23 @@
+﻿
+var builder = WebApplication.CreateBuilder(args);
+
+internal class WebApplication
+{
+    internal static object CreateBuilder(string[] args)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+// Configure services if needed
+// builder.Services.Add...
+
+var app = builder.Build();
+
+// Enable HTTPS redirection
+app.UseHttpsRedirection();
+
+// Configure endpoints
+app.MapGet("/", () => "Hello, HTTPS!");
+
+app.Run();
